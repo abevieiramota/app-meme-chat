de682d7c3d93c667085ef931979b4d409ec4b1e9
Abelardo Vieira Mota <abevieiramota@gmail.com>
2016-12-03 20:16:29 -0200

    adiciona spring mvc

---
+++ src/main/java/br/com/abevieiramota/memechat/conf/AppWebConfiguration.java
@@ -0,0 +1,73 @@
+package br.com.abevieiramota.memechat.conf;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.format.datetime.DateFormatter;
+import org.springframework.format.datetime.DateFormatterRegistrar;
+import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import br.com.abevieiramota.memechat.controller.HomeController;
+import br.com.abevieiramota.memechat.dao.MessageDao;
+
+@EnableWebMvc
+@ComponentScan(basePackageClasses = { HomeController.class, MessageDao.class })
+public class AppWebConfiguration extends WebMvcConfigurerAdapter {
+
+	@Bean
+	// viewName é transformado em <prefix> + viewName + <sufix>
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+
+		resolver.setExposedContextBeanNames("carrinhoCompras");
+
+		return resolver;
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("/WEB-INF/messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		// TODO: 1s para ambiente de desenvolvimento
+		messageSource.setCacheSeconds(1);
+
+		return messageSource;
+	}
+
+	@Bean
+	public FormattingConversionService mvcConversionService() {
+		DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+		DateFormatterRegistrar registrar = new DateFormatterRegistrar();
+		registrar.setFormatter(new DateFormatter("dd/MM/yyyy"));
+		registrar.registerFormatters(conversionService);
+
+		return conversionService;
+	}
+
+	@Bean
+	public MultipartResolver multipartResolver() {
+		return new StandardServletMultipartResolver();
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
---
+++ src/main/java/br/com/abevieiramota/memechat/model/Message.java
@@ -0,0 +1,14 @@
+package br.com.abevieiramota.memechat.model;
+
+public class Message {
+
+	private String content;
+	
+	public Message(String content) {
+		this.content = content;
+	}
+	
+	public String getContent() {
+		return this.content;
+	}
+}
