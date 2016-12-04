ea7e41c204cc8a8eef572967dedce982ec4d33b5
Abelardo Vieira Mota <abevieiramota@gmail.com>
2016-12-04 11:13:24 -0200

    add Meme id autogeneration

--- src/main/java/br/com/abevieiramota/memechat/model/Meme.java
+++ src/main/java/br/com/abevieiramota/memechat/model/Meme.java
@@ -1,15 +1,23 @@
 package br.com.abevieiramota.memechat.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 
 @Entity
 public class Meme {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Lob
+	private byte[] img;
 
 	public void generateImg() {
+		
 	}
 
 	public Long getId() {
