2780b6ecd018931af26d9846b2fc11b23e51fe74
Abelardo Vieira Mota <abevieiramota@gmail.com>
2016-12-04 18:05:05 -0200

    1 connection per method

--- src/main/java/br/com/abevieiramota/memechat/dao/MemeDao.java
+++ src/main/java/br/com/abevieiramota/memechat/dao/MemeDao.java
@@ -19,100 +19,101 @@ import org.springframework.stereotype.Repository;
 // https://jdbc.postgresql.org/documentation/81/binary-data.html
 public class MemeDao {
 
-	private Connection conn;
-
 	@Autowired
-	public MemeDao(DataSource ds) throws SQLException {
-
-		this.conn = ds.getConnection();
-	}
+	private DataSource ds;
 
 	public InputStream find(long id) {
-		try {
-			this.conn.setAutoCommit(false);
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-
-		LargeObjectManager lobj = null;
-		LargeObject obj = null;
-		try {
-			lobj = ((org.postgresql.PGConnection) this.conn).getLargeObjectAPI();
-			try(PreparedStatement stmt = this.conn.prepareStatement("SELECT bytes FROM meme WHERE id = ?")) {
-				stmt.setLong(1, id);
-				ResultSet rs = stmt.executeQuery();
-				if(!rs.next()) {
-					throw new IllegalArgumentException("meme not found");
-				}
-				long oid = rs.getLong(1);
-				obj = lobj.open(oid, LargeObjectManager.READ);
-				
-				byte buf[] = new byte[obj.size()];
-				obj.read(buf, 0, obj.size());
-				
-				return new ByteArrayInputStream(buf);
+		try (Connection conn = this.ds.getConnection()) {
+			try {
+				conn.setAutoCommit(false);
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
 			}
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		} finally {
-			if (obj != null) {
+
+			LargeObjectManager lobj = null;
+			LargeObject obj = null;
+			try {
+				lobj = ((org.postgresql.PGConnection) conn).getLargeObjectAPI();
+				try (PreparedStatement stmt = conn.prepareStatement("SELECT bytes FROM meme WHERE id = ?")) {
+					stmt.setLong(1, id);
+					ResultSet rs = stmt.executeQuery();
+					if (!rs.next()) {
+						throw new IllegalArgumentException("meme not found");
+					}
+					long oid = rs.getLong(1);
+					obj = lobj.open(oid, LargeObjectManager.READ);
+
+					byte buf[] = new byte[obj.size()];
+					obj.read(buf, 0, obj.size());
+
+					return new ByteArrayInputStream(buf);
+				}
+			} finally {
+				if (obj != null) {
+					try {
+						obj.close();
+					} catch (SQLException e) {
+						throw new RuntimeException(e);
+					}
+				}
 				try {
-					obj.close();
+					conn.setAutoCommit(true);
 				} catch (SQLException e) {
 					throw new RuntimeException(e);
 				}
 			}
-			try {
-				this.conn.setAutoCommit(true);
-			} catch (SQLException e1) {
-				throw new RuntimeException(e1);
-			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
 		}
 
 	}
 
 	public void add(InputStream imgAsStream, long id) {
-		try {
-			this.conn.setAutoCommit(false);
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-
-		LargeObjectManager lobj = null;
-		LargeObject obj = null;
-		long oid;
-		try {
-			lobj = ((org.postgresql.PGConnection) this.conn).getLargeObjectAPI();
-			oid = lobj.createLO(LargeObjectManager.READ | LargeObjectManager.WRITE);
-			obj = lobj.open(oid, LargeObjectManager.WRITE);
-
-			byte buf[] = new byte[2048];
-			int s;
-			while ((s = imgAsStream.read(buf, 0, 2048)) > 0) {
-				obj.write(buf, 0, s);
+		try (Connection conn = this.ds.getConnection()) {
+			try {
+				conn.setAutoCommit(false);
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
 			}
 
-			try (PreparedStatement pstmt = this.conn.prepareStatement("INSERT INTO meme VALUES (?, ?)")) {
-				pstmt.setLong(1, id);
-				pstmt.setLong(2, oid);
-				pstmt.executeUpdate();
-				this.conn.commit();
-			}
-		} catch (SQLException | IOException e) {
-			throw new RuntimeException(e);
-		} finally {
-			if (obj != null) {
+			LargeObjectManager lobj = null;
+			LargeObject obj = null;
+			long oid;
+			try {
+				lobj = ((org.postgresql.PGConnection) conn).getLargeObjectAPI();
+				oid = lobj.createLO(LargeObjectManager.READ | LargeObjectManager.WRITE);
+				obj = lobj.open(oid, LargeObjectManager.WRITE);
+
+				byte buf[] = new byte[2048];
+				int s;
+				while ((s = imgAsStream.read(buf, 0, 2048)) > 0) {
+					obj.write(buf, 0, s);
+				}
+
+				try (PreparedStatement pstmt = conn.prepareStatement("INSERT INTO meme VALUES (?, ?)")) {
+					pstmt.setLong(1, id);
+					pstmt.setLong(2, oid);
+					pstmt.executeUpdate();
+					conn.commit();
+				}
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			} finally {
+				if (obj != null) {
+					try {
+						obj.close();
+					} catch (SQLException e) {
+						throw new RuntimeException(e);
+					}
+				}
 				try {
-					obj.close();
+					conn.setAutoCommit(true);
 				} catch (SQLException e) {
 					throw new RuntimeException(e);
 				}
 			}
-			try {
-				this.conn.setAutoCommit(true);
-			} catch (SQLException e1) {
-				throw new RuntimeException(e1);
-			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
 		}
 	}
 }
